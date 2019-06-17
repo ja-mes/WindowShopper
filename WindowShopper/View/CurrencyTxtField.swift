9bbebf73a0f8e3ad3462c50f5a5f9430e84aeb69
James Brown <jamezbrown100@gmail.com>
2019-06-17 12:05:33 -0500

    Custom button class

---
+++ WindowShopper/View/CurrencyTxtField.swift
@@ -0,0 +1,32 @@
+//
+//  CurrencyTxtField.swift
+//  WindowShopper
+//
+//  Created by James Brown on 6/15/19.
+//  Copyright © 2019 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTxtField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        
+        layer.cornerRadius = 5.0
+        
+        textAlignment = .center
+        
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+
+            attributedPlaceholder = place
+            
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+
+    }
+
+}
