483a61bcea822c61687b3add09198371188b6c9f
James Brown <jamezbrown100@gmail.com>
2019-06-17 12:13:42 -0500

    second btn

--- WindowShopper/View/CurrencyTxtField.swift
+++ WindowShopper/View/CurrencyTxtField.swift
@@ -8,9 +8,17 @@
 
 import UIKit
 
+@IBDesignable
 class CurrencyTxtField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        customizeView()
+    }
     override func awakeFromNib() {
+        customizeView()
+    }
+    
+    func customizeView() {
         super.awakeFromNib()
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         
@@ -21,12 +29,11 @@ class CurrencyTxtField: UITextField {
         
         if let p = placeholder {
             let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
-
+            
             attributedPlaceholder = place
             
             textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
         }
-
     }
 
 }
